15c934f63bc2ab49155c22e51f3f76fd90f3f85b
rajnishk1997 <rajnishkaur.optum@gmail.com>
2024-06-12 15:16:25 +0530

    Audit Filter

--- dao/AuditTrailRepository.java
+++ dao/AuditTrailRepository.java
@@ -1,6 +1,8 @@
 package com.optum.dao;
 
 import com.optum.entity.AuditTrail;
+
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,5 +12,5 @@ import org.springframework.stereotype.Repository;
 public interface AuditTrailRepository extends JpaRepository<AuditTrail, Long> {
 	
 	 Page<AuditTrail> findAllByOrderByTimestampDesc(Pageable pageable);
-	
+	 Page<AuditTrail> findAll(Specification<AuditTrail> spec, Pageable pageable);
 }
