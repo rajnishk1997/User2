45b94c07a7542e5432ea1c48dcfaddde59e68e55
rajnishk1997 <rajnishkaur.optum@gmail.com>
2024-07-12 15:56:19 +0530

    GPP JSON 28

--- dao/GppJson28Dao.java
+++ dao/GppJson28Dao.java
@@ -10,5 +10,8 @@ public interface GppJson28Dao extends JpaRepository<RxSotGppData, String> {
 
 	 @Query(value = "SELECT gpp_json28 FROM rxcl_audit.rx_sot_gpp_data WHERE uid = :uid", nativeQuery = true)
 	    String findGppJson28ByUid(@Param("uid") String uid);
+
+	  @Query(value = "SELECT planc_code_override FROM rxcl_audit.rx_sot_gpp_data WHERE uid = :uid", nativeQuery = true)
+    String findPlancCodeOverrideByUid(@Param("uid") String uid);
 }
 
--- service/GppJson28Service.java
+++ service/GppJson28Service.java
@@ -22,82 +22,89 @@ public class GppJson28Service {
     @Autowired
     private ObjectMapper objectMapper;
 
-    public List<GppJson28FieldValidationResponse> getGppJson28ByUid(String uid) {
-        String gppJson28Str = repository.findGppJson28ByUid(uid);
-        List<Map<String, Object>> gppJson28List = parseJson(gppJson28Str);
-        List<Map<String, Object>> filteredList = filterByG(gppJson28List);
-        return comparePairs(filteredList);
-    }
 
-    private List<Map<String, Object>> parseJson(String jsonStr) {
-        try {
-            return objectMapper.readValue(jsonStr, new TypeReference<List<Map<String, Object>>>() {});
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to parse gpp_json28", e);
+        public List<GppJson28FieldValidationResponse> getGppJson28ByUid(String uid) {
+            String gppJson28Str = repository.findGppJson28ByUid(uid);
+            String plancCodeOverrideStr = repository.findPlancCodeOverrideByUid(uid);
+            
+            List<Map<String, Object>> gppJson28List = parseJson(gppJson28Str);
+            List<Map<String, Object>> plancCodeOverrideList = parseJson(plancCodeOverrideStr);
+            
+            Set<String> distinct00001Values = extractDistinct00001Values(plancCodeOverrideList);
+            List<Map<String, Object>> filteredList = filterByG(gppJson28List);
+            
+            return comparePairs(filteredList, distinct00001Values);
         }
-    }
 
-    private List<Map<String, Object>> filterByG(List<Map<String, Object>> gppJson28List) {
-        return gppJson28List.stream()
-                .filter(item -> "02".equals(item.get("G")))
-                .collect(Collectors.toList());
-    }
-
-    private List<GppJson28FieldValidationResponse> comparePairs(List<Map<String, Object>> filteredList) {
-        Map<String, Map<String, Object>> rMap = new HashMap<>();
-        Map<String, Map<String, Object>> sMap = new HashMap<>();
-
-//      If rMap contains an entry with "ADAECD": "BRM26NJ R22", it will look for a corresponding entry in sMap where "ADAECD" ends with "S22".
-//      Similarly, an entry with "ADAECD": "BRM26NJ RT" in rMap would be compared with "ADAECD" ending with "SP" in sMap.
-        for (Map<String, Object> item : filteredList) {
-            String adaecd = (String) item.get("ADAECD");
-            if (adaecd.endsWith("R") || adaecd.endsWith("RT") || adaecd.endsWith("R2") || adaecd.endsWith("R3")) {
-                rMap.put(adaecd, item);
-            } else if (adaecd.endsWith("S") || adaecd.endsWith("SP") || adaecd.endsWith("S2") || adaecd.endsWith("S3")) {
-                sMap.put(adaecd, item);
-            } else if (adaecd.matches(".*R\\d+$")) {
-                String sKey = adaecd.replaceFirst("R(\\d+)$", "S$1");
-                sMap.put(sKey, item);
-            } else if (adaecd.matches(".*S\\d+$")) {
-                String rKey = adaecd.replaceFirst("S(\\d+)$", "R$1");
-                rMap.put(rKey, item);
+        private List<Map<String, Object>> parseJson(String jsonStr) {
+            try {
+                return objectMapper.readValue(jsonStr, new TypeReference<List<Map<String, Object>>>() {});
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to parse JSON", e);
             }
         }
 
-        List<GppJson28FieldValidationResponse> responses = new ArrayList<>();
-//      It iterates over entries in rMap.
-//      For each entry (rEntry), it determines the corresponding sKey in sMap by replacing "R" with "S" and "RT" with "SP" in rKey.
-//      If sMap contains the sKey, it calls compareFields method to compare the JSON objects (rEntry.getValue() and sMap.get(sKey)).
-        for (Map.Entry<String, Map<String, Object>> rEntry : rMap.entrySet()) {
-            String rKey = rEntry.getKey();
-            String sKey = rKey.replace("R", "S").replace("RT", "SP");
-            if (sMap.containsKey(sKey)) {
-                responses.add(compareFields(rEntry.getValue(), sMap.get(sKey)));
+        private Set<String> extractDistinct00001Values(List<Map<String, Object>> plancCodeOverrideList) {
+            return plancCodeOverrideList.stream()
+                    .map(item -> (String) item.get("00001"))
+                    .collect(Collectors.toSet());
+        }
+
+        private List<Map<String, Object>> filterByG(List<Map<String, Object>> gppJson28List) {
+            return gppJson28List.stream()
+                    .filter(item -> "02".equals(item.get("G")))
+                    .collect(Collectors.toList());
+        }
+
+        private List<GppJson28FieldValidationResponse> comparePairs(List<Map<String, Object>> filteredList, Set<String> distinct00001Values) {
+            List<GppJson28FieldValidationResponse> responses = new ArrayList<>();
+
+            for (String value : distinct00001Values) {
+                List<Map<String, Object>> matchingEntries = findMatchingEntries(filteredList, value);
+                
+                for (Map<String, Object> entry : matchingEntries) {
+                    GppJson28FieldValidationResponse response = compareFields(entry);
+                    responses.add(response);
+                }
             }
+
+            return responses;
         }
 
-        return responses;
-    }
-
-    private GppJson28FieldValidationResponse compareFields(Map<String, Object> rFields, Map<String, Object> sFields) {
-        GppJson28FieldValidationResponse response = new GppJson28FieldValidationResponse();
-        Map<String, Json28FieldValidation> validationMap = new HashMap<>();
-
-        for (Map.Entry<String, Object> rEntry : rFields.entrySet()) {
-            String key = rEntry.getKey();
-            String rValue = String.valueOf(rEntry.getValue());
-            String sValue = sFields.containsKey(key) ? String.valueOf(sFields.get(key)) : null;
-            String validationStatus = rValue.equals(sValue) ? "true" : "false";
-
-            Json28FieldValidation validation = new Json28FieldValidation();
-            validation.setSName(key);
-            validation.setSValue(sValue);
-            validation.setValidationStatus(validationStatus);
-
-            validationMap.put(key, validation);
+        private List<Map<String, Object>> findMatchingEntries(List<Map<String, Object>> gppJson28List, String value) {
+            return gppJson28List.stream()
+                    .filter(item -> {
+                        String adbotx = (String) item.get("ADBOTX");
+                        return adbotx.endsWith("S") || adbotx.endsWith("SP");
+                    })
+                    .collect(Collectors.toList());
         }
 
-        response.setGppJson28Fields(validationMap);
-        return response;
-    }
+        private GppJson28FieldValidationResponse compareFields(Map<String, Object> entry) {
+            GppJson28FieldValidationResponse response = new GppJson28FieldValidationResponse();
+            Map<String, GppJson28FieldValidationResponse.Json28FieldValidation> validationMap = new HashMap<>();
+
+            Set<String> allKeys = new HashSet<>();
+            allKeys.addAll(entry.keySet());
+
+            for (String key : allKeys) {
+                String entryValue = entry.containsKey(key) ? String.valueOf(entry.get(key)) : "";
+                String validationStatus = "false";
+
+                if (!entryValue.isEmpty()) {
+                    validationStatus = "true";
+                }
+
+                GppJson28FieldValidationResponse.Json28FieldValidation fieldValidation = new GppJson28FieldValidationResponse.Json28FieldValidation();
+                fieldValidation.setValidationStatus(validationStatus);
+                fieldValidation.setsValue(key);
+                fieldValidation.setrValue(entryValue);
+
+                validationMap.put(key, fieldValidation);
+            }
+
+            response.setGppJson28Fields(validationMap);
+            return response;
+        }
+    
 }
